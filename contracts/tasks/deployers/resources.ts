80b26ff302a10dad8dfa59e23f282a80b1529afa
Mark Toda <toda.mark@gmail.com>
2021-09-12 18:25:50 -0700

    feat: add gitignore

---
+++ contracts/tasks/deployers/resources.ts
@@ -0,0 +1,32 @@
+import { task, types } from 'hardhat/config'
+import { TaskArguments } from 'hardhat/types'
+
+task('deploy:Resources')
+  .setAction(async function (_args: TaskArguments, hre) {
+    const marz = '0xd0ba8b19b0f5e25c11ed233302e75794c9d3142b';
+    console.log(
+      `Deploying Marz Resources with: \marz ${marz}`,
+    )
+
+    const factory = await hre.ethers.getContractFactory('MarzResources')
+    const resources = await hre.upgrades.deployProxy(factory, [marz], {
+      initializer: 'initialize(address)',
+    })
+
+    try {
+      await hre.run('verify:Resources', { address: resources.address })
+    } catch (e) {
+      console.log('Unable to verify on etherscan', e)
+    }
+  })
+
+task('verify:Resources', 'Verifies on etherscan')
+  .addParam('address', 'the contract address', undefined, types.string, false)
+  .setAction(async function (args: TaskArguments, hre) {
+    const { address } = args
+
+    await hre.run('verify:verify', {
+      address,
+      constructorArguments: [],
+    })
+  })
